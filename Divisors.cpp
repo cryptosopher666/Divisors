ab4e8e07c361879ac3377cd13581dd157b83c82b
Mohammad <m.mirgolbabaei666@gmail.com>
2023-04-30 10:40:21 +0330

    Create Divisors.cpp

---
+++ Divisors.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+main(){
+	int a;
+	cout<<"Enter number: ";
+	cin>>a;
+	cout<<"Divisors = ";
+	for(int i=1;i<=a;i++){
+		if(a%i==0)
+		cout<<i<<'\t';
+	}
+	getch();
+}
